ca550e48dd1a8c3349d536c61cb12a1674953445
arielrando <ariel_rando@hotmail.com.ar>
2017-02-17 15:44:49 -0300

    agragadas comparaciones de esquemas
    -- ver si la cantidad de esquemas coinciden en las 2 bd
    -- ver los esquemas que comparten el mismo nombre
    -- ver los esquemas que tiene la bd1 pero no la bd2
    -- ver los esquemas que tiene la bd2 pero no la bd1

--- query de comparacion.sql
+++ query de comparacion.sql
@@ -18,3 +18,24 @@ on N1.TABLE_NAME=M1.TABLE_NAME COLLATE Latin1_General_CS_AS where M1.TABLE_NAME 
 select N1.TABLE_NAME, M1.TABLE_NAME from AdventureWorks2014.INFORMATION_SCHEMA.TABLES N1
 right join AdventureWorks.INFORMATION_SCHEMA.TABLES M1
 on N1.TABLE_NAME=M1.TABLE_NAME COLLATE Latin1_General_CS_AS where N1.TABLE_NAME is null;
+
+-- ver si la cantidad de esquemas es la misma en las 2 bases
+select case when 
+(select COUNT(*) from AdventureWorks2014.INFORMATION_SCHEMA.SCHEMATA)=
+(select COUNT(*) from AdventureWorksClone2.INFORMATION_SCHEMA.SCHEMATA) 
+then 'iguales' else 'diferentes'end;
+
+-- ver todos los esquemas del mismo nombre
+select N1.SCHEMA_NAME, M1.SCHEMA_NAME from AdventureWorks2014.INFORMATION_SCHEMA.SCHEMATA N1
+inner join AdventureWorks.INFORMATION_SCHEMA.SCHEMATA M1
+on N1.SCHEMA_NAME=M1.SCHEMA_NAME COLLATE Latin1_General_CS_AS;
+
+-- ver todos los esquemas que no estan en la bd 2
+select N1.SCHEMA_NAME, M1.SCHEMA_NAME from AdventureWorks2014.INFORMATION_SCHEMA.SCHEMATA N1
+left join AdventureWorks.INFORMATION_SCHEMA.SCHEMATA M1
+on N1.SCHEMA_NAME=M1.SCHEMA_NAME COLLATE Latin1_General_CS_AS where M1.SCHEMA_NAME is null;
+
+-- ver todos los esquemas que no estan en la bd 1
+select N1.SCHEMA_NAME, M1.SCHEMA_NAME from AdventureWorks2014.INFORMATION_SCHEMA.SCHEMATA  N1
+right join AdventureWorks.INFORMATION_SCHEMA.SCHEMATA  M1
+on N1.SCHEMA_NAME=M1.SCHEMA_NAME COLLATE Latin1_General_CS_AS where N1.SCHEMA_NAME is null;
