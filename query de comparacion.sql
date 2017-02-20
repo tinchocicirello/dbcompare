5751d48aae2a308d4ba351fb89e101333e8812a1
arielrando <ariel_rando@hotmail.com.ar>
2017-02-19 21:53:22 -0300

    query para la comparacion de tablas (3º parte)
    -- se modificaron las tablas de ejemplo, ahora contienen restricciones
    -- se agregaron las direcciones de las cuales se extrae informacion de
    las tablas
    -- se creo una query para comparar si la columnas en las 2 tablas son
    null o no y coinciden
    -- se creo una query que revisa todas las primary key, foreign key y
    uniques de las tablas en las 2 bases de datos (se puede modificar para
    que tire de una tabla en especifico o una columna)

--- query de comparacion.sql
+++ query de comparacion.sql
@@ -67,8 +67,10 @@ close cursor1
 deallocate cursor1
 
 --IMPORTANTE!!!!!
---Query de comparacion, ahora devuelve las coincidencias en columnas si la tabla coincide en nombre
---Poner entre /*--*/ la query anterior para no generar errores
+------------------------------------------------------------------------------------------------
+--creacion de dos bases de datos para su comparacion, la tabla1 es igual en las 2, la tabla2--
+--tiene columnas distintas, la tabla3 no existe en la base de datos destino-------------------
+----------------------------------------------------------------------------------------------
 
 create database ejemplo1
 use ejemplo1 
@@ -76,13 +78,15 @@ use ejemplo1
 create table tabla1(
 columna1 int,
 columna2 varchar(50),
-columna3 date
+columna3 date,
+columna4 int
 )
 
 create table tabla2(
 columna1 int,
 columna2 varchar(50),
-columna3 date
+columna3 date,
+columna4 int
 )
 
 create table tabla3(
@@ -91,19 +95,42 @@ columna2 varchar(50),
 columna3 date
 )
 
+ALTER TABLE Tabla1 ALTER COLUMN columna1 INTEGER NOT NULL
+alter table tabla1 add constraint PK_tabla1_columna1 primary key (columna1)
+alter table tabla1 add constraint UN_tabla1_columna2 unique (columna2)
+alter table tabla1 add constraint CK_tabla1_columna3 check (columna3>'1-1-2015')
+ALTER TABLE Tabla1 ALTER COLUMN columna4 INTEGER NOT NULL
+alter table tabla1 add constraint DF_tabla1_columna4 default 1 for columna4 
+
+ALTER TABLE tabla3 ALTER COLUMN columna1 INTEGER NOT NULL
+alter table tabla3 add constraint PK_tabla3_columna1 primary key  (columna1)
+
+ALTER TABLE tabla2 ALTER COLUMN columna1 INTEGER NOT NULL
+alter table tabla2 add constraint FK_tabla2_columna1_tabla1_columna1 foreign key (columna1) references tabla1 (columna1) 
+alter table tabla2 add constraint FK_tabla2_columna1_tabla3_columna1 foreign key (columna1) references tabla3 (columna1)
+alter table tabla2 add constraint UN_tabla2_columna2 unique (columna2)
+alter table tabla2 add constraint DF_tabla2_columna3 default '1-1-2015' for columna3
+
+-- OJO!!!
+drop table tabla1;
+drop table tabla2;
+drop table tabla3;
+
 create database ejemplo2
 use ejemplo2 
 
 create table tabla1(
 columna1 int,
 columna2 varchar(50),
-columna3 date
+columna3 date,
+columna4 int
 )
 
 create table tabla2(
 columna1 int,
-columna2 int,
-columna3 int
+columna2 varchar(50),
+columna3 date,
+columna4 varchar(50)
 )
 
 create table tabla4(
@@ -111,6 +138,32 @@ columna1 int,
 columna2 varchar(50),
 columna3 date
 )
+
+ALTER TABLE Tabla1 ALTER COLUMN columna1 INTEGER NOT NULL
+alter table tabla1 add constraint PK_tabla1_columna1 primary key (columna1)
+alter table tabla1 add constraint UN_tabla1_columna2 unique (columna2)
+alter table tabla1 add constraint CK_tabla1_columna3 check (columna3>'1-1-2015')
+ALTER TABLE Tabla1 ALTER COLUMN columna4 INTEGER NOT NULL
+alter table tabla1 add constraint DF_tabla1_columna4 default 1 for columna4 
+
+ALTER TABLE tabla4 ALTER COLUMN columna1 INTEGER NOT NULL
+alter table tabla4 add constraint PK_tabla4_columna1 primary key  (columna1)
+
+ALTER TABLE tabla2 ALTER COLUMN columna1 INTEGER NOT NULL
+alter table tabla2 add constraint FK_tabla2_columna1_tabla1_columna1 foreign key (columna1) references tabla1 (columna1) 
+alter table tabla2 add constraint FK_tabla2_columna1_tabla4_columna1 foreign key (columna1) references tabla4 (columna1)
+alter table tabla2 add constraint UN_tabla2_columna2 unique (columna2)
+alter table tabla2 add constraint DF_tabla2_columna3 default '3-2-2015' for columna3
+
+--OJO!!!
+drop table tabla1;
+drop table tabla2;
+drop table tabla4;
+
+----------------------------------------------------------------------------------------------
+--Bloque para comparar las tablas y las columnas de las 2 bases-------------------------------
+----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
 
 use master
 
@@ -162,3 +215,79 @@ end
 
 close cursor1
 deallocate cursor1
+
+----------------------------------------------------------------------------------------------
+--direcciones donde se guarda informacion sobre las bases de datos----------------------------
+----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
+
+select * from ejemplo1.information_schema.CONSTRAINT_COLUMN_USAGE
+select * from ejemplo1.information_schema.CONSTRAINT_TABLE_USAGE
+select * from ejemplo1.information_schema.KEY_COLUMN_USAGE
+select * from ejemplo2.INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+select * from ejemplo1.INFORMATION_SCHEMA.CHECK_CONSTRAINTS
+select * from ejemplo2.INFORMATION_SCHEMA.CHECK_CONSTRAINTS
+select * from ejemplo1.information_schema.COLUMNS
+select * from ejemplo1.INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS
+select * from ejemplo1.INFORMATION_SCHEMA.COLUMN_PRIVILEGES
+select * from ejemplo1.information_schema.COLUMNS
+select * from ejemplo1.INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+
+----------------------------------------------------------------------------------------------
+--funcion para encontrar si la columna admite nulls o no en las dos bases---------------------
+----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
+
+
+if (select IS_NULLABLE from ejemplo1.information_schema.COLUMNS where TABLE_NAME='tabla1' and COLUMN_NAME='columna1')=
+(select IS_NULLABLE from ejemplo2.information_schema.COLUMNS where TABLE_NAME='tabla1' and COLUMN_NAME='columna3')
+print 'iguales'
+else
+print 'no iguales'
+
+----------------------------------------------------------------------------------------------
+--compara todas las primary key, foreing key y uniques de las dos bases (puede modificarse----
+--para mostrar coincidencias en en una tabla o en una columna)--------------------------------
+----------------------------------------------------------------------------------------------
+
+if exists (select CONSTRAINT_NAME from ejemplo1.INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+/*where TABLE_NAME='tabla3' and COLUMN_NAME='columna1'*/)
+begin
+
+declare @nombreTemporal nvarchar(50)
+declare @tipoTemporal nvarchar(50)
+
+declare cursor3 cursor for
+select ejemplo1.INFORMATION_SCHEMA.KEY_COLUMN_USAGE.CONSTRAINT_NAME,
+ejemplo1.INFORMATION_SCHEMA.TABLE_CONSTRAINTS.CONSTRAINT_TYPE
+from ejemplo1.INFORMATION_SCHEMA.KEY_COLUMN_USAGE inner join
+ejemplo1.INFORMATION_SCHEMA.TABLE_CONSTRAINTS on 
+ejemplo1.INFORMATION_SCHEMA.KEY_COLUMN_USAGE.CONSTRAINT_NAME
+=ejemplo1.INFORMATION_SCHEMA.TABLE_CONSTRAINTS.CONSTRAINT_NAME
+/*where ejemplo1.INFORMATION_SCHEMA.KEY_COLUMN_USAGE.TABLE_NAME='tabla3' 
+and ejemplo1.INFORMATION_SCHEMA.KEY_COLUMN_USAGE.COLUMN_NAME='columna1'*/
+
+open cursor3
+
+fetch next from cursor3 into @nombreTemporal,@tipoTemporal
+
+while @@FETCH_STATUS=0
+			begin
+			if exists (select CONSTRAINT_NAME,CONSTRAINT_TYPE from 
+			ejemplo2.INFORMATION_SCHEMA.TABLE_CONSTRAINTS where CONSTRAINT_NAME=@nombreTemporal
+			and CONSTRAINT_TYPE=@tipoTemporal)
+			begin
+				print @nombreTemporal+' '+@tipoTemporal+' existe en las 2 bases'
+			end
+			else
+			begin
+			print  @nombreTemporal+' '+@tipoTemporal+' no existe en la base destino'
+			end
+				fetch next from cursor3 into @nombreTemporal,@tipoTemporal
+			end
+
+close cursor3
+deallocate cursor3
+end
+else
+print 'no hay nada'
